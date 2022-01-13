b760a9de7be22896136ad7f8a3952bb25b40e7df
Ali Salehi <ali.zeiss.com>
2022-01-13 11:40:54 -0800

    Do some important changes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
 
 # Main file
+"""
+git init
+git status
 
+git commit
+
+"""
 
 if __name__ == "__main__":
 	print("Staring ..")
+	#a lot of stuff here, r
+	#eady to be packed and commited
+	
+	# add more stuff
